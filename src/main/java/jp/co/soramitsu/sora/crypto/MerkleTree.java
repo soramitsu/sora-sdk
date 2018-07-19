d5202c98dbc9d5c3380ce3b648eda03407affb30
Bogdan Vaneev <warchantua@gmail.com>
2018-07-19 12:45:51 +0300

    Remove getTree
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- src/main/java/jp/co/soramitsu/sora/crypto/MerkleTree.java
+++ src/main/java/jp/co/soramitsu/sora/crypto/MerkleTree.java
@@ -5,17 +5,14 @@ import lombok.Value;
 @Value
 public class MerkleTree {
 
-  // [root] [intermediate nodes] [leafs]
+  /**
+   * This is binary tree and it is stored "by levels", so first level is root and stored
+   * as tree[0], next level (2 items) is stored in tree[1] and tree[2], and so on.
+   *
+   * Scheme: [root] [intermediate nodes] [leafs]
+   */
   byte[][] tree;
 
-  /**
-   * Getter for the tree. Tree is stored "by levels", so first level is root and stored
-   * as tree[0], next level (2 items) is stored in tree[1] and tree[2], and so on.
-   * @return tree represented as [root] [intermediate nodes] [leafs]
-   */
-  public byte[][] getTree() {
-    return tree;
-  }
 
   /**
    * @return merkle root hash
