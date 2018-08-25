745722eb250bfbee5ffcf5519b4554106ad4a566
Bogdan Vaneev <warchantua@gmail.com>
2018-08-25 11:04:58 +0300

    Fix typo
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- src/main/java/jp/co/soramitsu/sora/crypto/json/flattener/DeflattenVisitor.java
+++ src/main/java/jp/co/soramitsu/sora/crypto/json/flattener/DeflattenVisitor.java
@@ -5,14 +5,14 @@ public interface DeflattenVisitor {
   /**
    * Triggered when parser finds next array key.
    *
-   * @param index index in array that is found
+   * @param index - index in array that is found
    */
   void visitArrayKey(Integer index);
 
   /**
    * Triggered when parser finds next dict key.
    *
-   * @param key dict key that is found
+   * @param key - dict key that is found
    */
   void visitDictKey(String key);
 }
