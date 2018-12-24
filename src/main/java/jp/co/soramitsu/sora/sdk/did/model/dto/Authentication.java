68467fd9f02a25a86d1c0cfec03fe1b0a9391d13
rogachevsn <rogachevsn55@gmail.com>
2018-12-24 15:40:25 +0300

    fix tests

--- src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/Authentication.java
+++ src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/Authentication.java
@@ -1,14 +1,19 @@
 package jp.co.soramitsu.sora.sdk.did.model.dto;
 
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.MINIMAL_CLASS;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jp.co.soramitsu.sora.sdk.did.model.Executable;
 import jp.co.soramitsu.sora.sdk.did.model.dto.authentication.AuthenticationExecutor;
+import jp.co.soramitsu.sora.sdk.did.model.dto.authentication.Ed25519Sha3Authentication;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-@JsonTypeInfo(use = MINIMAL_CLASS, property = "type")
+@JsonTypeInfo(use = NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Ed25519Sha3Authentication.class, name = "Ed25519Sha3Authentication")
+})
 @EqualsAndHashCode
 @ToString
 public abstract class Authentication implements Executable<AuthenticationExecutor> {
--- src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/PublicKey.java
+++ src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/PublicKey.java
@@ -1,16 +1,21 @@
 package jp.co.soramitsu.sora.sdk.did.model.dto;
 
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.MINIMAL_CLASS;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jp.co.soramitsu.sora.sdk.did.model.Executable;
+import jp.co.soramitsu.sora.sdk.did.model.dto.publickey.Ed25519Sha3VerificationKey;
 import jp.co.soramitsu.sora.sdk.did.model.dto.publickey.PublicKeyExecutor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
-@JsonTypeInfo(use = MINIMAL_CLASS, property = "type")
+@JsonTypeInfo(use = NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Ed25519Sha3VerificationKey.class, name = "Ed25519Sha3VerificationKey")
+})
 @AllArgsConstructor
 @Data
 public abstract class PublicKey implements Executable<PublicKeyExecutor> {
