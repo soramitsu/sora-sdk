61a86a8d3c687cb03db67fc69dc35e28ce58298e
jiexa <malyshevalexey8@gmail.com>
2019-01-31 17:35:45 +0300

    configured object mapper

--- src/main/java/jp/co/soramitsu/sora/sdk/json/JsonUtil.java
+++ src/main/java/jp/co/soramitsu/sora/sdk/json/JsonUtil.java
@@ -1,6 +1,7 @@
 package jp.co.soramitsu.sora.sdk.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
@@ -14,7 +15,8 @@ public class JsonUtil {
     return new ObjectMapper()
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .disable(SerializationFeature.WRITE_DATES_WITH_ZONE_ID)
-        .setDefaultPropertyInclusion(Include.NON_NULL);
+        .setDefaultPropertyInclusion(Include.NON_NULL)
+        .configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
   }
 
   public static <T> T deepClone(T obj, Class<T> type) throws IOException {
