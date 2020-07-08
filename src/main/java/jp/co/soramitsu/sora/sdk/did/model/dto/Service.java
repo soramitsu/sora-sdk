9d6493b51f0c20d1608345d28a89e74607edaa0a
rkharisov <50765419+rkharisov@users.noreply.github.com>
2020-07-08 16:34:18 +0300

    Changed json type info from CLASS to NAME and default class to deser as GenericService OBP-3 (#76)

--- src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/Service.java
+++ src/main/java/jp/co/soramitsu/sora/sdk/did/model/dto/Service.java
@@ -1,17 +1,20 @@
 package jp.co.soramitsu.sora.sdk.did.model.dto;
 
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.CLASS;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 import static java.util.Arrays.stream;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.net.URL;
 import jp.co.soramitsu.sora.sdk.did.model.Executable;
+import jp.co.soramitsu.sora.sdk.did.model.dto.service.GenericService;
 import jp.co.soramitsu.sora.sdk.did.model.dto.service.ServiceExecutor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
-@JsonTypeInfo(use = CLASS, property = "type")
+@JsonTypeInfo(use = NAME, property = "type", defaultImpl = GenericService.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
 public abstract class Service implements Executable<ServiceExecutor> {
