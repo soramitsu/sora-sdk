e3ceea5a36142e2635071b56f93e7704046ccba2
rkharisov <rinat@soramitsu.co.jp>
2021-06-02 18:35:20 +0300

    Made custom date-time  validation implementation thread-safe OBP-170

--- src/main/java/jp/co/soramitsu/sora/sdk/did/validation/ISO8601DateTimeFormatter.java
+++ src/main/java/jp/co/soramitsu/sora/sdk/did/validation/ISO8601DateTimeFormatter.java
@@ -1,24 +1,23 @@
 package jp.co.soramitsu.sora.sdk.did.validation;
 
+import static java.time.ZoneOffset.UTC;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 import java.util.TimeZone;
+import lombok.val;
 
 public class ISO8601DateTimeFormatter {
 
   private ISO8601DateTimeFormatter() {
   }
 
-  private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-
-  static {
-    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-  }
+  private static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
   public static Date parse(String value) throws ParseException {
-    return sdf.parse(value);
+    return createNewFormatter().parse(value);
   }
 
   public static String format(String time) {
@@ -30,6 +29,12 @@ public class ISO8601DateTimeFormatter {
   }
 
   public static String format(Date time) {
-    return sdf.format(time);
+    return createNewFormatter().format(time);
+  }
+
+  private static SimpleDateFormat createNewFormatter() {
+    val sdf =new SimpleDateFormat(PATTERN);
+    sdf.setTimeZone(TimeZone.getTimeZone(UTC));
+    return sdf;
   }
 }
